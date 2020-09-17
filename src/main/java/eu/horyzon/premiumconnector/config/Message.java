90df833f8a830300b8f644a0cc1fff9a9195c745
Corenb <Corenb@users.noreply.github.com>
2020-09-17 09:58:36 +0200

    Fix Message issues
    Add default message for prefix
    Fix getField for notPremiumError message

--- src/main/java/eu/horyzon/premiumconnector/config/Message.java
+++ src/main/java/eu/horyzon/premiumconnector/config/Message.java
@@ -6,22 +6,7 @@ import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.config.Configuration;
 
 public enum Message {
-	PREFIX("prefix", ""),
-	MYSQL_ERROR("mySQLError",
-			"|-\r\n" + "  &c          Database error!\r\n" + "  &cPlease retry later or contact an administrator"),
-	NOT_PREMIUM_ERROR("announceReward",
-			"|-\r\n" + "  &2     You're not the owner of this account\r\n"
-					+ "  &2Please change your username to join our server!"),
-	MOJANG_SERVER_ERROR("mojangServerError",
-			"|-\r\n" + "  &c       Authentification error\r\n"
-					+ "  &cMojang's servers are currently down, please retry later!"),
-	RATE_LIMIT("rateLimit",
-			"|-\r\n  &c       Authentification overload\r\n  &cAuthentificate process is currently overload. Please retry in one minute"),
-	WARN_COMMAND("warnCommand",
-			"%prefix% &c&lWARNING &6This command should &lonly &6be invoked if you are the owner of this paid minecraft account. Type &a/premium &6command again to confirm."),
-	PREMIUM_COMMAND("premiumCommand",
-			"%prefix% &6You are now &lPremium &6you need to reconnect you to the server to show correcly you skin."),
-	ALREADY_PREMIUM("alreadyPremium", "%prefix% &cYou are already a &6&lPremium &cuser.");
+	PREFIX("prefix", "&8[&6PremiumConnector&8] &6"), MYSQL_ERROR("mySQLError", "&cDatabase error!\r\n&cPlease retry later or contact an administrator"), NOT_PREMIUM_ERROR("notPremiumError", "&2You're not the owner of this account\r\n&2Please change your username to join our server!"), MOJANG_SERVER_ERROR("mojangServerError", "&cAuthentification error\r\n&cMojang's servers are currently down, please retry later!"), RATE_LIMIT("rateLimit", "&cAuthentification overload\r\n&cAuthentificate process is currently overload. Please retry in one minute"), WARN_COMMAND("warnCommand", "%prefix% &c&lWARNING &6This command should &lonly &6be invoked if you are the owner of this paid minecraft account. Type &a/premium &6command again to confirm."), PREMIUM_COMMAND("premiumCommand", "%prefix% &6You are now &lPremium &6you need to reconnect you to the server to show correcly you skin."), ALREADY_PREMIUM("alreadyPremium", "%prefix% &cYou are already a &6&lPremium &cuser.");
 
 	private final String key;
 	private String message;
